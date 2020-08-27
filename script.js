b8973df58c5cd3a5dba85012b323f5b6c8639be1
Philip Turkiewicz <philip.turkiewicz@live.ca>
2020-08-26 23:23:35 -0400

    minor css tweaks, working in contact tabs

--- script.js
+++ script.js
@@ -19,6 +19,34 @@ site.closeForm = () => {
    })
 }
 
+site.changeTabs = () => {
+   $('#contact').on('click', '.tab', function() {
+      console.log('click', $(this), $(this).prev("input[type='radio']").val())
+      if ($(this).prev("input[type='radio']").val() === 'contactTab') {
+         $('.displayed').html(`
+            <form action="https://formspree.io/xwkrjjnn" method="POST">
+   
+                  <label for="name" class="srOnly">Name:</label>
+                  <input type="text" name="name" class="name" id="name" placeholder="Name" required>
+   
+                  <label for="email" class="srOnly">Email:</label>
+                  <input type="email" name="email" class="email" id="email" placeholder="Email" required>
+   
+                  <label for="subject" class="srOnly">Subject:</label>
+                  <input type="text" name="subject" class="subject" id="subject" placeholder="Subject" required>
+   
+                  <label for="message" class="srOnly">Your message:</label>
+                  <textarea name="message" class="message" id="message" placeholder="What can I do for you?" required></textarea>
+   
+                  <button type="submit" class="button">Finished</button>
+               </form>
+         `)
+      } else {
+         $('.displayed').html('Calendly Tab')
+      }
+   })
+}
+
 site.headerAnimation = () => {
    $('.headerText p:first-child').fadeTo(3000, 1);
    $('h1').delay(2200).fadeTo(3000, 1);
@@ -37,6 +65,7 @@ site.init = () => {
    site.headerAnimation();
    site.showForm();
    site.closeForm();
+   site.changeTabs();
 }
 
 
